913410bddd00d285a1d5196c9260d8f4f6d4bc3c
rosalina_teixeira_oliveira <rosalina.oliveira@banqi.com>
2022-07-01 14:04:52 -0300

    nova função no rover

--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -1,10 +1,12 @@
+import java.lang.reflect.Field
+
 class Rover(
    var position: Position = Position()
 
 
 ) {
 
-    fun print(){
+    fun print() {
 
         println("${position.print()}")
     }
@@ -12,18 +14,42 @@ class Rover(
     fun control(command: Command): Position {
         when (command) {
             Command.L -> position.left()
-                //println("Spin 90 degrees left")
+            //println("Spin 90 degrees left")
 
             Command.R -> position.right()
-                //println("Spin 90 degrees right")
+            //println("Spin 90 degrees right")
 
             Command.M -> position.move()
-                //println("Move forward one grid point")
+            //println("Move forward one grid point")
 
-             else -> {
-            print("Command not implemented")
-        }
+            else -> {
+                print("Command not implemented")
+            }
         }
         return this.position
+    }
+
+    fun instruction(value: String) {
+
+        var listInstruction: List<String> = value.split(", ")
+        var strInitialPosition = listInstruction[0]
+        var strCommand = listInstruction [1]
+        var xyString = strInitialPosition!!.split(" ")
+        var x = xyString[0].toInt()
+        var y = xyString[1].toInt()
+        var direction = Direction.valueOf(xyString[2])
+        var position = Position(x, y, direction)
+        var rover = Rover(position)
+        rover.print()
+
+        val listStr: List<String> = strCommand.map { it.toString() }
+
+        for (strCommand in listStr) {
+            println(strCommand)
+            var command = Command.valueOf(strCommand)
+
+            rover.control(command)
+            rover.print()
+        }
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,27 +4,11 @@ fun main() {
     var initialPosition: Position = Position(3, 3, Direction.E)
     var rover: Rover = Rover(initialPosition)
 
+
     println("start")
-    rover.print()
-    rover.control(command = Command.M)
-    rover.print()
-    rover.control(command = Command.M)
-    rover.print()
-    rover.control(command = Command.R)
-    rover.print()
-    rover.control(command = Command.M)
-    rover.print()
-    rover.control(command = Command.M)
-    rover.print()
-    rover.control(command = Command.R)
-    rover.print()
-    rover.control(command = Command.M)
-    rover.print()
-    rover.control(command = Command.R)
-    rover.print()
-    rover.control(command = Command.R)
-    rover.control(command = Command.M)
-    rover.print()
+
+    rover.instruction("3 3 E, MMRMMRMRRM")
+
 
 }
 
